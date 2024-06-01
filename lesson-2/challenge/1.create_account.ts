4a4835bd8115069a551d256e9e33ca50198e3713
quocBao23 <131477339+quocBao23@users.noreply.github.com>
2024-06-01 23:30:35 +0700

    Update 1.create_account.ts

--- lesson-2/challenge/1.create_account.ts
+++ lesson-2/challenge/1.create_account.ts
@@ -1,26 +1,90 @@
-/**
- * Demonstrates how to generate a new random Solana keypair or load an existing keypair from a file.
- */
 import dotenv from "dotenv";
-import { loadKeypairFromFile } from "./lib/helpers";
-import { Keypair } from "@solana/web3.js";
+import {
+  explorerURL,
+  loadKeypairFromFile,
+  printConsoleSeparator,
+} from "./lib/helpers";
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  TransactionMessage,
+  VersionedTransaction,
+  clusterApiUrl,
+} from "@solana/web3.js";
 
 dotenv.config();
 
 (async () => {
+  const endpoint = clusterApiUrl("devnet");
+  const connection = new Connection(endpoint, "confirmed");
 
-  
-    // Generate a random keypair
-    const keypair = Keypair.generate();
-  
-    console.log(`The public key is: `, keypair.publicKey.toBase58());
-    console.log(`The secret key is: `, keypair.secretKey);
-  
-    // Load a keypair from a file
-    const localKp = loadKeypairFromFile(process.env.LOCAL_PAYER_JSON_ABSPATH as string);
-    console.log(`The public key is: `, localKp.publicKey.toBase58());
 
-  
-  
-    
-  })();
+  const senderKeypair = loadKeypairFromFile(
+    process.env.LOCAL_PAYER_JSON_ABSPATH as string
+  );
+
+  /**
+   * create a simple instruction (using web3.js) to create an account
+   */
+  const testWalletKeypair = Keypair.generate();
+
+  const space = 0; // on-chain space to allocated (in number of bytes)
+
+  // request the cost (in lamports) to allocate `space` number of bytes on chain
+  const balanceForRentExemption =
+    await connection.getMinimumBalanceForRentExemption(space);
+
+  // create this simple instruction using web3.js helper function
+  const createTestAccountIx = SystemProgram.createAccount({
+    // `fromPubkey` - this account will need to sign the transaction
+    fromPubkey: senderKeypair.publicKey,
+    // `newAccountPubkey` - the account address to create on chain
+    newAccountPubkey: testWalletKeypair.publicKey,
+    // lamports to store in this account
+    lamports: balanceForRentExemption,
+    // total space to allocate
+    space,
+    // the owning program for this account
+    programId: SystemProgram.programId,
+  });
+
+  const recentBlockhash = await connection
+    .getLatestBlockhash()
+    .then((res) => res.blockhash);
+
+  // create a transaction message
+  const message = new TransactionMessage({
+    payerKey: senderKeypair.publicKey,
+    recentBlockhash,
+    instructions: [
+      // create the test wallet's account on chain
+      createTestAccountIx,
+    ],
+  }).compileToV0Message();
+
+  /**
+   * try changing the order of the instructions inside of the message above...
+   * see what happens :)
+   */
+
+  // create a versioned transaction using the message
+  const tx = new VersionedTransaction(message);
+
+  // console.log("tx before signing:", tx);
+
+  // sign the transaction with our needed Signers (e.g. `senderKeypair` and `testWalletKeypair`)
+  tx.sign([senderKeypair, testWalletKeypair]);
+
+  // actually send the transaction
+  const sig = await connection.sendTransaction(tx);
+
+  /**
+   * display some helper text
+   */
+  printConsoleSeparator();
+
+  console.log("Transaction completed.");
+  console.log(explorerURL({ txSignature: sig }));
+})();
